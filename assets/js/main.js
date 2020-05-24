ae53cd6bb6f3fca8d4c3b944fbf0c532d33c706d
milano12345 <dmilano857@gmail.com>
2020-05-23 23:04:46 -0500

    moved posistioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,7 @@
 
   // Play initial animations on page load.
   $window.on("load", function () {
-    $("#loader").animate({
-      top: -200,
-    });
+    $("#loader").animate();
     window.setTimeout(function () {
       $body.removeClass("is-preload");
     }, 100);
