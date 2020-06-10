f2fbb38c645a7fe90834e22005c5aff682b76325
milano12345 <dmilano857@gmail.com>
2020-06-10 12:03:14 -0500

    commented out app.js

--- app.js
+++ app.js
@@ -1,65 +1,65 @@
-if (process.env.NODE_ENV !== "production") {
-  dotenv = require("dotenv");
-  dotenv.config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//   dotenv = require("dotenv");
+//   dotenv.config();
+// }
 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
-const bodyParser = require("body-parser");
-const helmet = require("helmet");
-const express = require("express");
+// const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+// const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+// const bodyParser = require("body-parser");
+// const helmet = require("helmet");
+// const express = require("express");
 
-const app = express();
-const fs = require("fs");
-app.use(helmet());
-app.use(express.static(__dirname));
-console.log(__dirname);
-app.set("views", __dirname + "/views");
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-app.set("view engine", "ejs");
+// const app = express();
+// const fs = require("fs");
+// app.use(helmet());
+// app.use(express.static(__dirname));
+// console.log(__dirname);
+// app.set("views", __dirname + "/views");
+// app.engine("html", require("ejs").renderFile);
+// app.set("view engine", "html");
+// app.set("view engine", "ejs");
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
+// app.use(bodyParser.json());
 
-app.get("/materials", (req, res) => {
-  fs.readFile("items.json", function (error, data) {
-    if (error) {
-      res.status(500).end();
-    } else {
-      res.render("store2.ejs", {
-        items: JSON.parse(data),
-        stripePublicKey: stripePublicKey,
-      });
-    }
-  });
-});
+// app.get("/materials", (req, res) => {
+//   fs.readFile("items.json", function (error, data) {
+//     if (error) {
+//       res.status(500).end();
+//     } else {
+//       res.render("store2.ejs", {
+//         items: JSON.parse(data),
+//         stripePublicKey: stripePublicKey,
+//       });
+//     }
+//   });
+// });
 
-app.get("/", function (req, res) {
-  res.render("index.html", {});
-});
+// app.get("/", function (req, res) {
+//   res.render("index.html", {});
+// });
 
-app.post("/purchase", (req, res) => {
-  fs.readFile("items.json", function (error, data) {
-    if (error) {
-      res.status(500).json({ message: "error" }).end();
-    } else {
-      console.log("purchased");
-      const itemsJson = JSON.parse(data);
-      const itemsArray = itemsJson.music.concat(itemsJson.merch);
-      let total = 0;
-      req.body.items.forEach(function (item) {
-        const itemJson = itemsArray.find(function (i) {
-          return i.id == item.id;
-        });
-        total = total + itemJson.price * item.quantity;
-      });
-    }
-  });
-});
+// app.post("/purchase", (req, res) => {
+//   fs.readFile("items.json", function (error, data) {
+//     if (error) {
+//       res.status(500).json({ message: "error" }).end();
+//     } else {
+//       console.log("purchased");
+//       const itemsJson = JSON.parse(data);
+//       const itemsArray = itemsJson.music.concat(itemsJson.merch);
+//       let total = 0;
+//       req.body.items.forEach(function (item) {
+//         const itemJson = itemsArray.find(function (i) {
+//           return i.id == item.id;
+//         });
+//         total = total + itemJson.price * item.quantity;
+//       });
+//     }
+//   });
+// });
 
-module.exports = app;
+// module.exports = app;
